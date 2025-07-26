2501308572670e8a6e9e16c5dd1f45da732bcee3
synrbb <192501796+synrbb@users.noreply.github.com>
2025-07-26 14:49:12 +0900

    Inject __instance and _damageSource

--- Patch_EntityAlive_DamageEntity.cs
+++ Patch_EntityAlive_DamageEntity.cs
@@ -6,31 +6,29 @@ namespace SoulslikeTargetBar
     [HarmonyPatch(nameof(EntityAlive.DamageEntity))]
     class Patch_EntityAlive_DamageEntity
     {
-        static void Postfix(EntityAlive __instance)
+        static void Postfix(EntityAlive __instance, int __result, DamageSource _damageSource)
         {
-            int strength = __instance.RecordedDamage.ModStrength;
-            if (strength <= 0)
+            if (__result <= 0)
             {
                 return;
             }
-            DamageSource source = __instance.RecordedDamage.Source;
-            switch (source.CreatorEntityId)
+            switch (_damageSource.CreatorEntityId)
             {
                 case -2:
                     // Blade Trap, SMG Auto Turret, Shotgun Auto Turret
                     return;
                 case -1:
-                    if (source.getEntityId() == -1)
+                    if (_damageSource.getEntityId() == -1)
                     {
                         // Burning, Bleeding, Electric Fence Post, Spike
                         return;
                     }
-                    if (source.ItemClass?.Name == "ammoDartIron")
+                    if (_damageSource.ItemClass?.Name == "ammoDartIron")
                     {
                         // Dart Trap
                         return;
                     }
-                    XUiC_SoulslikeTargetBar.ShowDamage(strength, __instance);
+                    XUiC_SoulslikeTargetBar.ShowDamage(__result, __instance);
                     return;
                 default:
                     // Robotic Sledge, Robotic Turret
