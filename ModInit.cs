5813784a9a0ac2fbc359ba7fe2a29f73f9c273ae
synrbb <192501796+synrbb@users.noreply.github.com>
2025-08-06 02:36:17 +0900

    Fix harmony Id

--- ModInit.cs
+++ ModInit.cs
@@ -6,7 +6,7 @@ namespace SoulslikeTargetBar
     {
         public void InitMod(Mod _modInstance)
         {
-            var harmony = new Harmony("com.example.SoulslikeTargetBar");
+            var harmony = new Harmony("io.github.synrbb.SoulslikeTargetBar");
             harmony.PatchAll();
         }
     }
